6b8448e97e47dda85e452d33d3f365ba91f03e2e
Simon Cropp <simon.cropp@gmail.com>
2020-02-24 12:37:25 +1100

    .

--- src/Verify.ImageMagick/VerifyImageMagick_Pdf.cs
+++ src/Verify.ImageMagick/VerifyImageMagick_Pdf.cs
@@ -1,42 +1,35 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using ImageMagick;
 using Verify;
 
 public static partial class VerifyImageMagick
 {
-    static ConcurrentExclusiveSchedulerPair scheduler = new ConcurrentExclusiveSchedulerPair();
+    static SynchronizationContext scheduler = new SynchronizationContext();
 
-    static async Task<ConversionResult> ConvertPdf(Stream stream, VerifySettings verifySettings)
+    static ConversionResult ConvertPdf(Stream stream, VerifySettings verifySettings)
     {
         var streams = new List<Stream>();
-
-        await Task.Factory.StartNew(() =>
+        scheduler.Send(state =>
+        {
+            // Settings the density to 300 dpi will create an image with a better quality
+            var magickSettings = new MagickReadSettings
             {
-                // Settings the density to 300 dpi will create an image with a better quality
-                var magickSettings = new MagickReadSettings
-                {
-                    Density = new Density(100, 100),
-                    Format = MagickFormat.Pdf,
-                };
-                using var images = new MagickImageCollection();
-                // Add all the pages of the pdf file to the collection
-                images.Read(stream, magickSettings);
+                Density = new Density(100, 100),
+                Format = MagickFormat.Pdf
+            };
+            using var images = new MagickImageCollection();
+            // Add all the pages of the pdf file to the collection
+            images.Read(stream, magickSettings);
 
-
-                foreach (var image in images)
-                {
-                    var memoryStream = new MemoryStream();
-                    image.Write(memoryStream, MagickFormat.Png);
-                    streams.Add(memoryStream);
-                }
-
-            },
-            CancellationToken.None,
-            TaskCreationOptions.DenyChildAttach,
-            scheduler.ExclusiveScheduler);
+            foreach (var image in images)
+            {
+                var memoryStream = new MemoryStream();
+                image.Write(memoryStream, MagickFormat.Png);
+                streams.Add(memoryStream);
+            }
+        }, null);
 
         return new ConversionResult(null, streams);
     }
