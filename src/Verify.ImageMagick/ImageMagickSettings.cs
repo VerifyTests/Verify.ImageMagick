f3b6b5be794a57c16b9a518b1e4cebfc2c4459d5
Simon Cropp <simon.cropp@gmail.com>
2020-02-24 14:00:04 +1100

    .

---
+++ src/Verify.ImageMagick/ImageMagickSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Verify
+{
+    public static class ImageMagickSettings
+    {
+        public static void PagesToInclude(this VerifySettings settings, int count)
+        {
+            Guard.AgainstNull(settings, nameof(settings));
+            settings.Data["ImageMagickPagesToInclude"] = count;
+        }
+
+        internal static bool GetPagesToInclude(this VerifySettings settings, [NotNullWhen(true)] out int? pages)
+        {
+            Guard.AgainstNull(settings, nameof(settings));
+            if (settings.Data.TryGetValue("ImageMagickPagesToInclude", out var value))
+            {
+                pages = (int?) value;
+                return true;
+            }
+
+            pages = null;
+            return false;
+        }
+    }
+}
--- src/Verify.ImageMagick/VerifyImageMagick_Pdf.cs
+++ src/Verify.ImageMagick/VerifyImageMagick_Pdf.cs
@@ -1,36 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Threading;
 using ImageMagick;
 using Verify;
 
 public static partial class VerifyImageMagick
 {
-    static SynchronizationContext scheduler = new SynchronizationContext();
-    static MagickImageCollection images = new MagickImageCollection();
-
     static ConversionResult ConvertPdf(Stream stream, VerifySettings verifySettings)
     {
         var streams = new List<Stream>();
-        scheduler.Send(state =>
+        // Settings the density to 300 dpi will create an image with a better quality
+        var magickSettings = new MagickReadSettings
         {
-            // Settings the density to 300 dpi will create an image with a better quality
-            var magickSettings = new MagickReadSettings
-            {
-                Density = new Density(100, 100),
-                Format = MagickFormat.Pdf
-            };
-            images.Clear();
-            // Add all the pages of the pdf file to the collection
-            images.Read(stream, magickSettings);
+            Density = new Density(100, 100),
+            Format = MagickFormat.Pdf
+        };
+        using var images = new MagickImageCollection();
+        images.Read(stream, magickSettings);
+        var count = images.Count;
+        if (verifySettings.GetPagesToInclude(out var pagesToInclude))
+        {
+            count = Math.Min(count, (int)pagesToInclude);
+        }
 
-            foreach (var image in images)
-            {
-                var memoryStream = new MemoryStream();
-                image.Write(memoryStream, MagickFormat.Png);
-                streams.Add(memoryStream);
-            }
-        }, null);
+        for (var index = 0; index < count; index++)
+        {
+            var image = images[index];
+            var memoryStream = new MemoryStream();
+            image.Write(memoryStream, MagickFormat.Png);
+            streams.Add(memoryStream);
+        }
 
         return new ConversionResult(null, streams);
     }
