d289f7d1c69c15930db7e1a7c90b07d31391410f
Simon Cropp <simon.cropp@gmail.com>
2020-02-25 15:11:50 +1100

    .

--- src/Verify.ImageMagick/VerifyImageMagick.cs
+++ src/Verify.ImageMagick/VerifyImageMagick.cs
@@ -6,13 +6,20 @@ public static partial class VerifyImageMagick
     public static void Initialize()
     {
         SharedVerifySettings.RegisterFileConverter("pdf", "png", ConvertPdf);
-        SharedVerifySettings.RegisterComparer("png", (stream1, stream2) =>
-        {
-            using var img1 = new MagickImage(stream1);
-            using var img2 = new MagickImage(stream2);
-            //https://imagemagick.org/script/command-line-options.php#metric
-            var diff = img1.Compare(img2, ErrorMetric.Fuzz);
-            return diff < .0001;
-        });
+        RegisterComparer();
+    }
+
+    public static void RegisterComparer(double threshold = .005, ErrorMetric metric = ErrorMetric.Fuzz)
+    {
+        SharedVerifySettings.RegisterComparer(
+            "png",
+            (stream1, stream2) =>
+            {
+                using var img1 = new MagickImage(stream1);
+                using var img2 = new MagickImage(stream2);
+                //https://imagemagick.org/script/command-line-options.php#metric
+                var diff = img1.Compare(img2, metric);
+                return diff < threshold;
+            });
     }
 }
