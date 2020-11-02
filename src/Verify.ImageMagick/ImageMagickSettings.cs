444b1326372a205b73b691ab1f218404280c5f7e
Simon Cropp <simon.cropp@gmail.com>
2020-11-02 19:57:52 +1100

    SettingsTask

--- src/Verify.ImageMagick/ImageMagickSettings.cs
+++ src/Verify.ImageMagick/ImageMagickSettings.cs
@@ -12,6 +12,13 @@ namespace VerifyTests
             settings.Context["ImageMagick.PagesToInclude"] = count;
         }
 
+        public static SettingsTask PagesToInclude(this SettingsTask settings, int count)
+        {
+            Guard.AgainstNull(settings, nameof(settings));
+            settings.CurrentSettings.PagesToInclude(count);
+            return settings;
+        }
+
         internal static bool GetPagesToInclude(this IReadOnlyDictionary<string, object> context, [NotNullWhen(true)] out int? pages)
         {
             Guard.AgainstNull(context, nameof(context));
@@ -30,6 +37,13 @@ namespace VerifyTests
             Guard.AgainstNull(settings, nameof(settings));
             Guard.AgainstNull(magickReadSettings, nameof(magickReadSettings));
             settings.Context["ImageMagick.MagickReadSettings"] = magickReadSettings;
+        }
+
+        public static SettingsTask MagickReadSettings(this SettingsTask settings, MagickReadSettings magickReadSettings)
+        {
+            Guard.AgainstNull(settings, nameof(settings));
+            settings.CurrentSettings.MagickReadSettings(magickReadSettings);
+            return settings;
         }
 
         internal static MagickReadSettings MagickReadSettings(this IReadOnlyDictionary<string, object> context)
