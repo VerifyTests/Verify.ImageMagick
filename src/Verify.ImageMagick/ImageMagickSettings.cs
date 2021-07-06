e5e7fff819b4cddef6bde07988c1e3d09149a2b6
Simon Cropp <simon.cropp@gmail.com>
2021-07-07 09:01:51 +1000

    bump version

--- src/Verify.ImageMagick/ImageMagickSettings.cs
+++ src/Verify.ImageMagick/ImageMagickSettings.cs
@@ -8,20 +8,17 @@ namespace VerifyTests
     {
         public static void PagesToInclude(this VerifySettings settings, int count)
         {
-            Guard.AgainstNull(settings, nameof(settings));
             settings.Context["ImageMagick.PagesToInclude"] = count;
         }
 
         public static SettingsTask PagesToInclude(this SettingsTask settings, int count)
         {
-            Guard.AgainstNull(settings, nameof(settings));
             settings.CurrentSettings.PagesToInclude(count);
             return settings;
         }
 
         internal static bool GetPagesToInclude(this IReadOnlyDictionary<string, object> context, [NotNullWhen(true)] out int? pages)
         {
-            Guard.AgainstNull(context, nameof(context));
             if (context.TryGetValue("ImageMagick.PagesToInclude", out var value))
             {
                 pages = (int) value;
@@ -34,21 +31,17 @@ namespace VerifyTests
 
         public static void MagickReadSettings(this VerifySettings settings, MagickReadSettings magickReadSettings)
         {
-            Guard.AgainstNull(settings, nameof(settings));
-            Guard.AgainstNull(magickReadSettings, nameof(magickReadSettings));
             settings.Context["ImageMagick.MagickReadSettings"] = magickReadSettings;
         }
 
         public static SettingsTask MagickReadSettings(this SettingsTask settings, MagickReadSettings magickReadSettings)
         {
-            Guard.AgainstNull(settings, nameof(settings));
             settings.CurrentSettings.MagickReadSettings(magickReadSettings);
             return settings;
         }
 
         internal static MagickReadSettings MagickReadSettings(this IReadOnlyDictionary<string, object> context)
         {
-            Guard.AgainstNull(context, nameof(context));
             if (context.TryGetValue("ImageMagick.MagickReadSettings", out var value))
             {
                 return (MagickReadSettings) value;
